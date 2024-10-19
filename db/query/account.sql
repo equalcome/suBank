03acab712d0cf8011941bdb3773576514938d880
equalcome <derozen1001@gmail.com>
2024-10-19 18:12:02 +0800

    first push

---
+++ db/query/account.sql
@@ -0,0 +1,48 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  owner,
+  balance,
+  currency
+) VALUES (
+  $1, $2 ,$3
+)
+RETURNING *;
+  
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- LIMIT $1：這表示最多返回 $1 行記錄，這裡的 $1 是一個佔位符，實際值在查詢執行時由使用者傳入。這讓查詢能夠靈活控制要返回的記錄數。
+
+-- OFFSET $2：這表示從第 $2 行開始返回結果，這裡的 $2 也是一個佔位符。這通常用於實現分頁功能，可以跳過前面指定的行數。
+
+-- LIMIT $1** 和 OFFSET $2： 這兩個佔位符可以用來實現查詢的分頁功能。舉例來說，LIMIT 10 和 OFFSET 20 將返回第 21 到第 30 條記錄，這使得查詢結果可以按頁分割和顯示。
+
+-- name: UpdateAccount :one
+UPDATE accounts 
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts 
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts 
+WHERE id = $1;
+
+
---
+++ db/sqlc/store.go
@@ -0,0 +1,139 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store provides all function to execute db queries and transactions
+type Store struct {
+    *Queries  // 繼承 Queries，能夠執行資料庫查詢
+    db *sql.DB  // 資料庫連接對象
+}
+
+
+//NewStore creates a new Store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+		Queries: New(db),
+	}
+}
+
+//execTx executes a function within a database
+//執行各種的數據庫操作
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error)error{
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+			// 短變量聲明
+			// if x := 10; x > 5 {
+			// 	fmt.Println("x is greater than 5")
+			// }
+            // err: 表示執行傳入的 fn(q) 函數時發生的錯誤。
+			// rbErr: 表示回滾事務時發生的錯誤
+		if rbErr := tx.Rollback(); rbErr != nil {                              
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	
+
+	return tx.Commit()
+}
+ 
+// TransferTxParams contains the input parameters of the transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID int64 `json:"to_account_id"`
+	Amount int64 `json:"amount"`
+}
+
+// TransferTxResult is the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer Transfer `json:"transfer"`
+	FromAccount Account `json:"from_account"`
+	ToAccount Account `json:"to_account"`
+	FromEntry Entry `json:"from_entry"`
+	ToEntry Entry `json:"to_entry"`
+}
+
+
+   
+
+//TransferTx performs a money transfer from one account to the other
+//It creates a transfer record, add account entries, and update accounts' balance within a single database transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		
+
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		
+		//誰id小誰先動，避免deadlock
+		if arg.FromAccountID < arg.ToAccountID {				
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+
+	}
+		return nil 
+	})
+
+	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error ) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID1,
+		Amount: amount1,
+	})
+	if err != nil { 
+		return //account1, account2, err //酷酷的go特色
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID2,
+		Amount: amount2,
+	})
+	return
+}
